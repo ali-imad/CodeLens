83932fa778d0e96541aff962a87a7b6dec5e62c8
Benny Li <cli66@student.ubc.ca>
2024-06-08 16:21:41 -0700

    feature: initial route set up for problems and mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,64 +1,33 @@
-import express, { Response, Request } from "express";
+// app.ts
+import express, { Response } from "express";
+import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import problemRouter from "./routes/problemRoutes";
 
-// Define types for Code Sample and Test entities
-interface CodeSample {
-  id: number;
-  code: string;
-  description: string;
-}
+dotenv.config();
 
-// Initialize Express app
 const app = express();
 const PORT: number = parseInt(process.env["PORT"] || "3000");
 
-// Middleware
+app.use(cors());
 app.use(bodyParser.json());
+
+// Connect to MongoDB
+const uri: string = process.env["MONGODB_URI"] || "";
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 // Routes
 app.get("/", (res: Response) => {
   res.send("Welcome to CodeLens API");
 });
 
-// Code Samples Routes
-const codeSamples: CodeSample[] = [];
-let codeSampleIdCounter: number = 1;
-
-app.get("/code-samples", (res: Response) => {
-  res.json(codeSamples);
-});
-
-app.post("/code-samples", (req: Request, res: Response) => {
-  const newCodeSample: CodeSample = req.body;
-  newCodeSample.id = codeSampleIdCounter++;
-  codeSamples.push(newCodeSample);
-  res.status(201).json(newCodeSample);
-});
-
-app.put("/code-samples/:id", (req: Request<{ id: string }>, res: Response) => {
-  const id: number = parseInt(req.params.id);
-  const index: number = codeSamples.findIndex((sample) => sample.id === id);
-  if (index === -1) {
-    res.status(404).send("Code Sample not found");
-  } else {
-    codeSamples[index] = { ...codeSamples[index], ...req.body };
-    res.json(codeSamples[index]);
-  }
-});
-
-app.delete(
-  "/code-samples/:id",
-  (req: Request<{ id: string }>, res: Response) => {
-    const id: number = parseInt(req.params.id);
-    const index: number = codeSamples.findIndex((sample) => sample.id === id);
-    if (index === -1) {
-      res.status(404).send("Code Sample not found");
-    } else {
-      codeSamples.splice(index, 1);
-      res.sendStatus(204);
-    }
-  }
-);
+app.use("/problems", problemRouter);
 
 // Start the server
 app.listen(PORT, () => {
---
+++ backend/src/routes/problemRoutes.ts
@@ -0,0 +1,81 @@
+import express, { Request, Response } from "express";
+import Problem, { IProblem } from "../models/Problem";
+
+const router = express.Router();
+
+// Retrieve all problems
+router.get("/", async (res: Response) => {
+  try {
+    const problems: IProblem[] = await Problem.find();
+    res.json(problems);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Retrieve a specific problem by ID
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const problemId: string | undefined = req.params["id"];
+    const problem: IProblem | null = await Problem.findById(problemId);
+    if (!problem) {
+      return res.status(404).json({ message: "Problem not found" });
+    }
+    return res.json(problem);
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Create a new problem
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const { title, difficulty, functionBody } = req.body;
+    const newProblem: IProblem = new Problem({
+      title,
+      difficulty,
+      functionBody,
+    });
+    await newProblem.save();
+    res.status(201).json(newProblem);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update an existing problem
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const problemId: string | undefined = req.params["id"];
+    const { title, difficulty, functionBody } = req.body;
+    const updatedProblem: IProblem | null = await Problem.findByIdAndUpdate(
+      problemId,
+      { title, difficulty, functionBody },
+      { new: true }
+    );
+    if (!updatedProblem) {
+      return res.status(404).json({ message: "Problem not found" });
+    }
+    return res.json(updatedProblem);
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete a problem
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const problemId: string | undefined = req.params["id"];
+    const deletedProblem: IProblem | null = await Problem.findByIdAndDelete(
+      problemId
+    );
+    if (!deletedProblem) {
+      return res.status(404).json({ message: "Problem not found" });
+    }
+    return res.json({ message: "Problem deleted successfully" });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
